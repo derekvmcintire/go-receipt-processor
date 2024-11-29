1ba43667c4896548cf7497a2b1ecd68d265851be
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-11-28 22:40:52 -0500

    Fix small bug in calculating alpha numeric characters (#13)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- internal/domain/receipt.go
+++ internal/domain/receipt.go
@@ -12,11 +12,11 @@ type Item struct {
 // Receipt represents a customer's receipt and its associated data.
 // It includes details about the retailer, purchase information, items, total amount, and points.
 type Receipt struct {
-	ID           string  // A unique identifier for the receipt.
-	Retailer     string  // The name of the retailer where the receipt was issued.
-	PurchaseDate string  // The date of purchase in YYYY-MM-DD format.
-	PurchaseTime string  // The time of purchase in HH:MM format (24-hour clock).
-	Items        []Item  // A list of items purchased, represented by the Item struct.
-	Total        float64 // The total amount for the receipt in currency units.
-	Points       int     // The points calculated based on the receipt's data.
+	ID           string // A unique identifier for the receipt.
+	Retailer     string // The name of the retailer where the receipt was issued.
+	PurchaseDate string // The date of purchase in YYYY-MM-DD format.
+	PurchaseTime string // The time of purchase in HH:MM format (24-hour clock).
+	Items        []Item // A list of items purchased, represented by the Item struct.
+	Total        string // The total amount for the receipt in currency units.
+	Points       int    // The points calculated based on the receipt's data.
 }
