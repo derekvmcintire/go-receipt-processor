9df78790b41ca7b1ce92a12ddc58e1905260af98
Derek McIntire <derekbmcintire@gmail.com>
2024-11-29 13:18:53 -0500

    add test cases for GetPoints

--- tests/application_test/receipt_service_test.go
+++ tests/application_test/receipt_service_test.go
@@ -65,3 +65,47 @@ func TestReceiptService_ProcessReceipt_ErrorInPointsCalculation(t *testing.T) {
 	mockPointsCalculator.AssertExpectations(t)
 	mockReceiptStore.AssertExpectations(t)
 }
+
+func TestReceiptService_GetPoints(t *testing.T) {
+	// Create mock objects
+	mockPointsCalculator := new(local_mocks.MockPointsCalculator)
+	mockReceiptStore := new(local_mocks.MockReceiptStore)
+
+	// Create the ReceiptService instance with the mocked dependencies
+	receiptService := application.NewReceiptService(mockPointsCalculator, mockReceiptStore)
+
+	// Prepare the test data
+	receipt := local_mocks.MockReceipt
+	receipt.Points = 50 // Set the expected points for the receipt
+
+	// Mock the behavior of Find method to return the receipt
+	mockReceiptStore.On("Find", "12345").Return(receipt, nil)
+
+	// Call the GetPoints method
+	points, err := receiptService.GetPoints("12345")
+
+	// Assertions
+	assert.NoError(t, err)      // No error should be returned
+	assert.Equal(t, 50, points) // Points should be 50
+	mockReceiptStore.AssertExpectations(t)
+}
+
+func TestReceiptService_GetPoints_ReceiptNotFound(t *testing.T) {
+	// Create mock objects
+	mockPointsCalculator := new(local_mocks.MockPointsCalculator)
+	mockReceiptStore := new(local_mocks.MockReceiptStore)
+
+	// Create the ReceiptService instance with the mocked dependencies
+	receiptService := application.NewReceiptService(mockPointsCalculator, mockReceiptStore)
+
+	// Mock the behavior of Find method to return an error (receipt not found)
+	mockReceiptStore.On("Find", "12345").Return(domain.Receipt{}, fmt.Errorf("receipt not found"))
+
+	// Call the GetPoints method
+	points, err := receiptService.GetPoints("12345")
+
+	// Assertions
+	assert.Error(t, err)       // Error should be returned
+	assert.Equal(t, 0, points) // Points should be 0
+	mockReceiptStore.AssertExpectations(t)
+}
